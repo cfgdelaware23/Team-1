2b7bfbc04799ffa305339661fff00addc3cc8b9e
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-29 19:29:54 -0400

    add event list component

--- frontend/cfg-app/src/App.js
+++ frontend/cfg-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import EventList from './components/Event.js'
+
 
 function App() {
   return (
     <div className="App">
       <h1> Upcoming Events </h1>
-
+      <EventList />
     </div>
   );
 }
--- frontend/cfg-app/src/components/Event.js
+++ frontend/cfg-app/src/components/Event.js
@@ -1,7 +1,6 @@
 function Event() {
     return (
         <div> 
-            <h1 className="weekly-schedule"> Upcoming Events </h1>
             <h2 className="event-name"> Event Title 1</h2>
             <p className="event-description"> Event Description</p>
             <p className="event-date"> Event Date</p>
@@ -11,3 +10,8 @@ function Event() {
     );
   }
   
+export default function EventList(){
+    return (
+        <Event />
+    )
+}
