0286beced48ebc09f79783684d683c23d3096038
Isaiah <isaiahnat10@gmail.com>
2023-09-29 19:33:37 -0400

    Fixed Conflict

--- api/Scheduler.py
+++ api/Scheduler.py
@@ -10,18 +10,5 @@ def jsonToPandas(var_name):
     file_name = env_vals[var_name]
     with urllib.request.urlopen(file_name) as url:
         raw_data = json.load(url)
-    # print(data)
-    # print(raw_data)
     return pd.json_normalize(raw_data)
     
-
-
-# input google sheet scraper
-
-# with open('books.json') as f:
-#     data = json.load(f)
-
-
-# df = pd.json_normalize()
-
-print(jsonToPandas("events_api"))
