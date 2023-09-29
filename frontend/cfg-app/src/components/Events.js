63347d54d45205be60ba08a864e494ae173745e1
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-29 18:50:47 -0400

    added event component

---
+++ frontend/cfg-app/src/components/Events.js
@@ -0,0 +1,13 @@
+function Event() {
+    return (
+        <div> 
+            <h1 className="weekly-schedule"> Upcoming Events </h1>
+            <h2 className="event-name"> Event Title 1</h2>
+            <p className="event-description"> Event Description</p>
+            <p className="event-date"> Event Date</p>
+            <p className="event-time"> Event Time</p>
+        </div>
+
+    );
+  }
+  
