22d3fb2d672d9d1455bac08fb9b869d9aa7ebe93
Isaiah <isaiahnat10@gmail.com>
2023-09-29 20:49:32 -0400

    Scheduler Updated

--- api/Scheduler.py
+++ api/Scheduler.py
@@ -10,5 +10,29 @@ def jsonToPandas(var_name):
     file_name = env_vals[var_name]
     with urllib.request.urlopen(file_name) as url:
         raw_data = json.load(url)
-    return pd.json_normalize(raw_data)
+    # print(raw_data)
+    return pd.json_normalize(raw_data["data"])
     
+def getDayHash(df):
+    output = {"Monday":{} , "Tuesday":{}, "Wednesday":{}, 
+              "Thursday":{}, "Friday":{}, "Saturday":{},
+              "Sunday":{}
+             }
+    for i, row in df.iterrows():
+        if i == 0:
+            continue
+        event = (row["event_name"], row["time"])
+        output[row["day"]][event] = []
+
+    return output
+
+
+def getSchedule(dayHash, availDf):
+    pass
+
+    
+df = jsonToPandas("events_api")
+print(getDayHash(df))
+# print(df["event_name"])
+# for ind in df.index:
+#     print(df['event_name'][ind])
