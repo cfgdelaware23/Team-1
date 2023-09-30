c2a48087c5c229c7807696e42c44dacb94c3915d
thamidachoudhury1 <thamida.choudhury@gmail.com>
2023-09-30 08:55:24 -0400

    SearchFunctionality

--- frontend/cfg-app/src/components/SearchBar.jsx
+++ frontend/cfg-app/src/components/SearchBar.jsx
@@ -1,14 +1,36 @@
 import React, {useState} from "react";
+import axios from 'axos'
 import "./SearchBar.css";
 export const SearchBar = () => {
     const [name, setName] = useState("")
+function volunteerForms() {
+    const [input, setInput] = useState('');
+}
+
+const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+};
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.get("http://127.0.0.1:5000/volunteer/one?input=${input}")
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+};
+
+
     return (
             <div className="input-section">
                 <i className="fa fa-search"></i>
                 <input 
                 placeholder="Type in Volunteers Name" 
-                value = {name}
-                onChange={(e) => setName(e.target.value)}
+                value = {input}
+                onChange={handleInputChange}
                 />
             </div>
     );
