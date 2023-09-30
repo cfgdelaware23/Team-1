b694bc82770e85dc65ee45f7e18455da72bb3fa8
Ann Panat <ann.panat@gmail.com>
2023-09-29 20:51:34 -0400

    Add button functionality

--- frontend/cfg-app/src/App.js
+++ frontend/cfg-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function clickMe() {
+  alert('Clicked');
 }
+export default function App() {
+    return(
 
-export default App;
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+
+        <div>
+        <button alt={"Add Events"} onClick={clickMe} style={{backgroundColor: "#e3faff", width: "150px", height: "100px", 
+        borderRadius: "15px", fontFamily: "Roboto", fontWeight: "bold", fontSize: "27px"}}>
+          Add Events
+        </button>
+        <p></p>
+        <button alt={"Add Events"} onClick={clickMe} style={{backgroundColor: "#e3faff", width: "150px", height: "100px", 
+        borderRadius: "15px", fontFamily: "Roboto", fontWeight: "bold", fontSize: "27px"}}>
+          Remove Events
+        </button>
+        </div>
+      </div>
+    );
+}
