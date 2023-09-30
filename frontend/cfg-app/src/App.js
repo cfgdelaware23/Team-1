24b24be93b2f152f93bf0040f7ae14da1244705a
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-30 04:47:18 -0400

    add search bar and routes

--- frontend/cfg-app/src/App.js
+++ frontend/cfg-app/src/App.js
@@ -4,11 +4,15 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EventList from './components/Event.js'
 import Search from './components/Button.js'
+import { SearchBar } from './components/SearchBar';
+import './App.css';
 import Form from './components/Form.js'
 import RedirectComponent from './components/RedirectComponent.js'
 
+
 function App() {
   return (
+    <div className='flexbox-container'>
   <Router>
       <div className="App">
         <h1> Upcoming Events </h1>
@@ -18,8 +22,12 @@ function App() {
             <Route path="/form" component={Form} />
             <Route path="/redirect" component={RedirectComponent} />
           </Routes>
+          <div className='search-section'>
+            <SearchBar />
+          </div>
       </div>
   </Router>
+  </div>
   );
 }
 
