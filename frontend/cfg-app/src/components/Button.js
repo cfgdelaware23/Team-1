5b22ce6122160fa645c3f1ee541b32eab1859ad7
Ronald Jabouin <14562+rj336@users.noreply.github.coecis.cornell.edu>
2023-09-29 21:17:51 -0400

    fixed alt text for button

--- frontend/cfg-app/src/components/Button.js
+++ frontend/cfg-app/src/components/Button.js
@@ -6,14 +6,14 @@ function clickMe() {
 export default function Button() {
     return(
 
-      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>	 
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>	 
     <div>
         <button alt={"Add Events"} onClick={clickMe} style={{backgroundColor: "#e3faff", width: "150px", height: "100px", 
         borderRadius: "15px", fontFamily: "Roboto", fontWeight: "bold", fontSize: "27px"}}>
           Add Events
         </button>
         <p></p>
-        <button alt={"Add Events"} onClick={clickMe} style={{backgroundColor: "#e3faff", width: "150px", height: "100px", 
+        <button alt={"Remove Events"} onClick={clickMe} style={{backgroundColor: "#e3faff", width: "150px", height: "100px", 
         borderRadius: "15px", fontFamily: "Roboto", fontWeight: "bold", fontSize: "27px"}}>
           Remove Events
         </button>
