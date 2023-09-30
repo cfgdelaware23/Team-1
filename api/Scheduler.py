bb1aad32ab7c4a9f58f2c68e269ca19bc5cdf15f
AkhilDeo <singingbird99@gmail.com>
2023-09-29 20:47:27 -0400

    Create barebones API structure

--- api/Scheduler.py
+++ api/Scheduler.py
@@ -2,7 +2,13 @@ import json
 import pandas as pd
 from dotenv import dotenv_values
 import urllib.request
+from flask import Flask, Blueprint
 
+schedule_api = Blueprint('schedule', __name__)
+
+@schedule_api.route("/", methods = ['GET'])
+def get_schedule():
+    return "GET Schedule"
 
 def jsonToPandas(var_name):
 
